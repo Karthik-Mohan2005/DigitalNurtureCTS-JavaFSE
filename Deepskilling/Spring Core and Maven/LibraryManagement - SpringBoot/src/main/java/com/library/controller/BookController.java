edaeff18bc4a1b2913b65cc652e3be8e95818039
unknown <karthik.mohan2210@gmail.com>
2026-06-28 20:55:01 +0530

    Implemented Spring Boot Application - Library Management

---
+++ Deepskilling/Spring Core and Maven/LibraryManagement - SpringBoot/src/main/java/com/library/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.library.controller;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository repository;
+    @GetMapping
+    public List<Book> getBooks() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/addBook")
+    public Book addBook(@RequestBody Book book) {
+        return repository.save(book);
+    }
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable int id, @RequestBody Book book) {
+        book.setId(id);
+        return repository.save(book);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable int id) {
+        repository.deleteById(id);
+        return "Book Deleted Successfully";
+    }
+}
